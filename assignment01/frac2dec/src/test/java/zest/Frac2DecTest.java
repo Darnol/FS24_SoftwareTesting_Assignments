83b49dbf0d70037d81c78b7946a20e682b41d478
Darnol <32634736+Darnol@users.noreply.github.com>
2024-03-12 16:43:22 +0100

    WIP : Add Coverage, todo: add JaCoCo

--- assignment01/frac2dec/src/test/java/zest/Frac2DecTest.java
+++ assignment01/frac2dec/src/test/java/zest/Frac2DecTest.java
@@ -1,5 +1,14 @@
 package zest;
 
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 class Frac2DecTest {
+ @Test
+ public void testValid() {
+  assertEquals(1,1);
+ }
+
  }
