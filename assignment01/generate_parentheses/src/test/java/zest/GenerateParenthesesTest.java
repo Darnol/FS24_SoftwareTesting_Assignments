12b8ea534db82845ad8c73bc8a05fca80fe0a55e
Darnol <32634736+Darnol@users.noreply.github.com>
2024-03-21 08:25:33 +0100

    generate_parentheses : Setup pom

--- assignment01/generate_parentheses/src/test/java/zest/GenerateParenthesesTest.java
+++ assignment01/generate_parentheses/src/test/java/zest/GenerateParenthesesTest.java
@@ -1,6 +1,12 @@
 package zest;
 
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
 class GenerateParenthesesTest {
 
+  @Test
+  public void testDummy() {
+    assertEquals(1,1);
+  }
 }
