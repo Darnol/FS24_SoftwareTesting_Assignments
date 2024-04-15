72c3c776b9484784fe84ab53eece176f19ec7574
Darnol <32634736+Darnol@users.noreply.github.com>
2024-04-15 17:06:38 +0200

    Setup pom.xml for MergeKSortedLists and SortedArray2BST

--- assignment02/MergeKSortedLists/src/test/java/zest/MergeKSortedListsTest.java
+++ assignment02/MergeKSortedLists/src/test/java/zest/MergeKSortedListsTest.java
@@ -1,6 +1,13 @@
 package zest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
 class MergeKSortedListsTest {
 
- 
+    @Test
+    public void test_dummy() {
+        assertTrue(true);
+    }
 }
