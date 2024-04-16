aae24dce63e637ca6f87734615cc6e2e67f17e12
Darnol <32634736+Darnol@users.noreply.github.com>
2024-04-16 16:41:42 +0200

    mergeKSortedLists : Done Task1

--- assignment02/MergeKSortedLists/Documentation.md
+++ assignment02/MergeKSortedLists/Documentation.md
@@ -1,8 +1,31 @@
 # MergeKSortedLists
 
 ## Task 1: Code Coverage
+Note: I did initially not test the pre-conditions, since this is part of Task 2 and 3.
+Note: The documentation does not specify at all what happens when the input is not a sorted linked list. I included a test to assert that a non-sorted input indeed will not be sorted. Otherwise I left this case open on purpose.
+
+I achieved 100% code coverage with the following tests from the test suite:
+- T1 - T6 : Check valid inputs for various specifications
+- T7 - T9 : Check invalid inputs (not sorted linked lists)
+
+Find a screenshot and the log in the Assets folder, starting with "task1_"
 
 ## Task 2: Designing Contracts
+Added a JavaDoc for the static method `mergeKLists` 
+
+### Pre-conditions
+
+- Pre1 : The number of all nodes in the input combined is in the range [0,10^4]
+- Pre2 : Each node value in the input is in the range [-10^4,10^4]
+- Pre3 : The input is not null
+- Pre4 : Each linked list of the input is sorted
+
+### Post-conditions
+- Post1 : The output is one single linked list
+
+### Invariants
+
+
 
 ## Task 3: Testing Contracts
 
--- assignment02/MergeKSortedLists/src/test/java/zest/MergeKSortedListsTest.java
+++ assignment02/MergeKSortedLists/src/test/java/zest/MergeKSortedListsTest.java
@@ -71,7 +71,6 @@ class MergeKSortedListsTest {
 
     @Test
     public void test_helpers() {
-
         List<Integer> exp = List.of(1,1,2,3,4,4,5,6);
         ListNode expected = createListNode(exp);
 
@@ -93,10 +92,170 @@ class MergeKSortedListsTest {
     }
 
     @Test
-    public void test_null_empty() {
+    public void T1_test_null_empty() {
         ListNode[] null_input = null;
         ListNode[] empty_input = new ListNode[0];
         assertNull(merger.mergeKLists(null_input));
         assertNull(merger.mergeKLists(empty_input));
+    }
+
+    @Test
+    public void T2_test_valid_singleInput_singleLink() {
+        /*
+        Test input where it is a single linked list with no link
+         */
+        List<Integer> exp = List.of(1);
+        ListNode expected = createListNode(exp);
+
+        List<Integer> i1 = List.of(1);
+
+        List<List<Integer>> inputs = List.of(i1);
+        ListNode[] inputs_listNode = new ListNode[inputs.size()];
+
+        for (int i = 0; i < inputs_listNode.length; i++) {
+            List<Integer> l = inputs.get(i);
+            inputs_listNode[i] = createListNode(l);
+        }
+
+        ListNode result = merger.mergeKLists(inputs_listNode);
+        assertEquals(getListNodeElements(expected), getListNodeElements(result));
+    }
+
+    @Test
+    public void T3_test_valid_singleInput_multiLink() {
+        /*
+        Test input where it is a single linked list with links
+         */
+        List<Integer> exp = List.of(3,1,2);
+        ListNode expected = createListNode(exp);
+
+        List<Integer> i1 = List.of(3,1,2);
+
+        List<List<Integer>> inputs = List.of(i1);
+        ListNode[] inputs_listNode = new ListNode[inputs.size()];
+
+        for (int i = 0; i < inputs_listNode.length; i++) {
+            List<Integer> l = inputs.get(i);
+            inputs_listNode[i] = createListNode(l);
+        }
+
+        ListNode result = merger.mergeKLists(inputs_listNode);
+        assertEquals(getListNodeElements(expected), getListNodeElements(result));
+    }
+
+    @Test
+    public void T4_test_valid_multiInput_singleLink() {
+        /*
+        Test input where it is multiple linked lists, each with no links
+         */
+        List<Integer> exp = List.of(10,20,30);
+        ListNode expected = createListNode(exp);
+
+        List<Integer> i1 = List.of(10);
+        List<Integer> i2 = List.of(30);
+        List<Integer> i3 = List.of(20);
+
+        List<List<Integer>> inputs = List.of(i1,i2,i3);
+        ListNode[] inputs_listNode = new ListNode[inputs.size()];
+
+        for (int i = 0; i < inputs_listNode.length; i++) {
+            List<Integer> l = inputs.get(i);
+            inputs_listNode[i] = createListNode(l);
+        }
+
+        ListNode result = merger.mergeKLists(inputs_listNode);
+        assertEquals(getListNodeElements(expected), getListNodeElements(result));
+    }
+
+    @Test
+    public void T5_test_valid_multiInput_mixLink() {
+        /*
+        Test input where it is multiple linked lists, mixing no links and links
+         */
+        List<Integer> exp = List.of(10,20,30);
+        ListNode expected = createListNode(exp);
+
+        List<Integer> i1 = List.of(20);
+        List<Integer> i2 = List.of(10, 30);
+
+        List<List<Integer>> inputs = List.of(i1,i2);
+        ListNode[] inputs_listNode = new ListNode[inputs.size()];
+
+        for (int i = 0; i < inputs_listNode.length; i++) {
+            List<Integer> l = inputs.get(i);
+            inputs_listNode[i] = createListNode(l);
+        }
+
+        ListNode result = merger.mergeKLists(inputs_listNode);
+        assertEquals(getListNodeElements(expected), getListNodeElements(result));
+    }
+
+    @Test
+    public void T6_test_valid_multiInput_multiLink() {
+        /*
+        Test input where it is multiple linked lists, all containing links
+         */
+        List<Integer> exp = List.of(10,20,30,40);
+        ListNode expected = createListNode(exp);
+
+        List<Integer> i1 = List.of(20,40);
+        List<Integer> i2 = List.of(10, 30);
+
+        List<List<Integer>> inputs = List.of(i1,i2);
+        ListNode[] inputs_listNode = new ListNode[inputs.size()];
+
+        for (int i = 0; i < inputs_listNode.length; i++) {
+            List<Integer> l = inputs.get(i);
+            inputs_listNode[i] = createListNode(l);
+        }
+
+        ListNode result = merger.mergeKLists(inputs_listNode);
+        assertEquals(getListNodeElements(expected), getListNodeElements(result));
+    }
+
+    @Test
+    public void T7_test_invalid_notSortedInput() {
+        /*
+        Test input that is a linked list, but not sorted
+         */
+        List<Integer> exp = List.of(10,30,20);
+        ListNode expected = createListNode(exp);
+
+        List<Integer> i1 = List.of(10);
+        List<Integer> i2 = List.of(30,20);
+
+        List<List<Integer>> inputs = List.of(i1,i2);
+        ListNode[] inputs_listNode = new ListNode[inputs.size()];
+
+        for (int i = 0; i < inputs_listNode.length; i++) {
+            List<Integer> l = inputs.get(i);
+            inputs_listNode[i] = createListNode(l);
+        }
+
+        ListNode result = merger.mergeKLists(inputs_listNode);
+        assertEquals(getListNodeElements(expected), getListNodeElements(result));
+    }
+
+    @Test
+    public void T8_test_invalid_notSortedInput_orderReversed() {
+        /*
+        Test input that is a linked list, but not sorted
+         */
+        List<Integer> exp = List.of(10,30,20);
+        ListNode expected = createListNode(exp);
+
+        List<Integer> i1 = List.of(30,20);
+        List<Integer> i2 = List.of(10);
+
+        List<List<Integer>> inputs = List.of(i1,i2);
+        ListNode[] inputs_listNode = new ListNode[inputs.size()];
+
+        for (int i = 0; i < inputs_listNode.length; i++) {
+            List<Integer> l = inputs.get(i);
+            inputs_listNode[i] = createListNode(l);
+        }
+
+        ListNode result = merger.mergeKLists(inputs_listNode);
+        assertEquals(getListNodeElements(expected), getListNodeElements(result));
     }
 }
