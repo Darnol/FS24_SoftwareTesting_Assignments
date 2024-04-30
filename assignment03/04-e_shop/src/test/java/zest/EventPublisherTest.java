5a851b722cd6098d5ee4508f0fa5287138530d2f
Darnol <32634736+Darnol@users.noreply.github.com>
2024-04-30 15:54:48 +0200

    WIP : Setup 04-e_shop

--- assignment03/04-e_shop/src/test/java/zest/EventPublisherTest.java
+++ assignment03/04-e_shop/src/test/java/zest/EventPublisherTest.java
@@ -10,4 +10,15 @@ class EventPublisherTest {
     void test_dummy() {
         assertTrue(true);
     }
+
+//    @Test
+//    void test_numberOfInvocations() {
+//
+//        // a real instance of EventPublisher and Order, no need to mock them
+//        EventPublisher eventPublisher = new EventPublisher();
+//        Order order = new Order("1", 100.0);
+//
+//        // Mock the two EventListener dependencies
+////        EventListener emailService = mock()
+//    }
 }
