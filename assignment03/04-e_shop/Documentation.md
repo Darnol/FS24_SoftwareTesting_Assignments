a7827689dea943be44c3ee46505d33268f9779a1
Darnol <32634736+Darnol@users.noreply.github.com>
2024-04-30 17:20:09 +0200

    WIP : 04 eshop

--- assignment03/04-e_shop/Documentation.md
+++ assignment03/04-e_shop/Documentation.md
@@ -1,10 +1,18 @@
 # 04-e_shop
 
 ### NOTE
-The task mentions in the description a method `publishOrderPlaced`.  
+- The task mentions in the description a method `publishOrderPlaced`.  
 The class `zest.EventPublisher` does not have a method with this name, but a method named `publishOrderToAllListeners`. I assume that this public method is referred to.
-
-Furthermore, I took the freedom to refactor the project strcuture, created the package `zest` and moved the classes into it.
+- Task C refers to the `MessageProcessor` class, which is not present in the source code of this exercise. I assume that this is a typo and the `EmailNotificationService` class is meant.
+Furthermore, I took the freedom to refactor the project structure, created the package `zest` and moved the classes into it.
 
 ### A. Number of invocations
+This is tested in the test `test_numberOfInvocations`. I created a real eventPublisher and order and mocked the two dependencies that inherit from the EventListener interface. Then i publish an order and verify that each service calls the method `onOrderPlaced` exactly once.
 
+### B. Content of invocations—ArgumentCaptor
+This is tested with `test_ArgumentCaptor` which verifies the content of the arguments.
+
+### C. Content of invocations—Increasing observability
+In order to 
+
+### Comparison
--- assignment03/04-e_shop/src/test/java/zest/EventPublisherTest.java
+++ assignment03/04-e_shop/src/test/java/zest/EventPublisherTest.java
@@ -1,8 +1,12 @@
 package zest;
 
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
+
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.Mockito.*;
 
 class EventPublisherTest {
 
@@ -11,14 +15,89 @@ class EventPublisherTest {
         assertTrue(true);
     }
 
-//    @Test
-//    void test_numberOfInvocations() {
-//
-//        // a real instance of EventPublisher and Order, no need to mock them
-//        EventPublisher eventPublisher = new EventPublisher();
-//        Order order = new Order("1", 100.0);
-//
-//        // Mock the two EventListener dependencies
-////        EventListener emailService = mock()
-//    }
+    @Test
+    void test_numberOfInvocations() {
+
+        // a real instance of EventPublisher and Order, no need to mock them
+        EventPublisher eventPublisher = new EventPublisher();
+        Order order = new Order("firstOrder", 1);
+
+        // Mock the two EventListener dependencies
+        EventListener emailService = mock(EmailNotificationService.class);
+        EventListener inventoryManager = mock(InventoryManager.class);
+
+        // Add the two listeners to our eventPublishers
+        eventPublisher.subscribe(emailService);
+        eventPublisher.subscribe(inventoryManager);
+
+        // Call the publish method
+        eventPublisher.publishOrderToAllListeners(order);
+
+        // Verify that each service called the onOrderPlaced method once
+        verify(emailService, times(1)).onOrderPlaced(order);
+        verify(inventoryManager, times(1)).onOrderPlaced(order);
+    }
+
+    @Test
+    void test_ArgumentCaptor() {
+
+        // a real instance of EventPublisher and Order, no need to mock them
+        EventPublisher eventPublisher = new EventPublisher();
+        Order order = new Order("firstOrder", 1);
+
+        // Mock the two EventListener dependencies
+        EventListener emailService = mock(EmailNotificationService.class);
+        EventListener inventoryManager = mock(InventoryManager.class);
+
+        // Add the two listeners to our eventPublishers
+        eventPublisher.subscribe(emailService);
+        eventPublisher.subscribe(inventoryManager);
+
+        // Call the publish method
+        eventPublisher.publishOrderToAllListeners(order);
+
+        // Create ArgumentCaptors instances
+        ArgumentCaptor<Order> emailServiceCaptor = ArgumentCaptor.forClass(Order.class);
+        ArgumentCaptor<Order> inventoryManagerCaptor = ArgumentCaptor.forClass(Order.class);
+
+        // Do not verify times, we did that already
+        // Verify the arguments of the order are correct
+        verify(emailService).onOrderPlaced(emailServiceCaptor.capture());
+        verify(inventoryManager).onOrderPlaced(inventoryManagerCaptor.capture());
+
+        // OrderId
+        assertEquals(order.getOrderId(), emailServiceCaptor.getValue().getOrderId());
+        assertEquals(order.getOrderId(), inventoryManagerCaptor.getValue().getOrderId());
+
+        // Amount
+        assertEquals(order.getAmount(), emailServiceCaptor.getValue().getAmount());
+        assertEquals(order.getAmount(), inventoryManagerCaptor.getValue().getAmount());
+
+    }
+
+    @Test
+    void test_enhancedObservability() {
+
+        // a real instance of EventPublisher and Order, no need to mock them
+        EventPublisher eventPublisher = new EventPublisher();
+        Order order = new Order("firstOrder", 1);
+
+        // Mock the two EventListener dependencies
+        EventListener emailService = mock(EmailNotificationService.class);
+        EventListener inventoryManager = mock(InventoryManager.class);
+
+        // Add the two listeners to our eventPublishers
+        eventPublisher.subscribe(emailService);
+        eventPublisher.subscribe(inventoryManager);
+
+        // Call the publish method
+        List<Order> publishedOrders = eventPublisher.publishOrderToAllListeners(order);
+
+        // Verify that the returned order objects from the publisher listeners are correct
+        for (Order o : publishedOrders) {
+            assertEquals(order.getOrderId(), o.getOrderId());
+            assertEquals(order.getAmount(), o.getAmount());
+        }
+    }
+
 }
