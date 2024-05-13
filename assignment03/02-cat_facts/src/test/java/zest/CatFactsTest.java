90574da192104c18d820f35fbd00f9bcd82b5cfb
nils.bohnenblust <nils.bohnenblust@weatherbound.com>
2024-05-13 10:36:29 +0200

    Create cat test file

---
+++ assignment03/02-cat_facts/src/test/java/zest/CatFactsTest.java
@@ -0,0 +1,19 @@
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+
+
+
+
+
+public class CatFactsTest {
+
+    @Test
+    public void kjhkj() {
+
+    }
+}
