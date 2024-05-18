294d458a122a3c36e1ac44201617bf51ec283fab
maximilianhuwyler <maximilian.huwyler@uzh.ch>
2024-05-18 17:05:54 +0200

    restructure exercise 7

---
+++ assignment03/07-payment_processing/src/test/java/zest/EventPublisherTest.java
@@ -0,0 +1,16 @@
+package zest;
+
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Collections;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.*;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.AfterEach;
+
+
+public class EventPublisherTest {
+
+}
